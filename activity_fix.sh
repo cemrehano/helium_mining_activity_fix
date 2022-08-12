39962011bbc210f44676afb7b1ad9c53e0a7bd97
cemrehano <111056538+cemrehano@users.noreply.github.com>
2022-08-12 13:34:44 +0300

    Update activity_fix.sh

--- activity_fix.sh
+++ activity_fix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-miner_name=$(docker exec miner miner print_keys|awk '/pubkey/ {split($1,a,"\""); print a[2]}')
 docker_name=$(docker ps -a|grep miner|awk -F" " '{print $NF}')
+miner_name=$(docker exec $docker_name miner print_keys|awk '/pubkey/ {split($1,a,"\""); print a[2]}')
 height=$(curl -s https://api.helium.io/v1/blocks/height | grep -Po '"height":[^}]+' | sed -e 's/^"height"://')
 last_poc_challenge=$(curl -s https://api.helium.io/v1/hotspots/$miner_name |grep -Po '"last_poc_challenge":[^\,]+' | sed -e 's/^"last_poc_challenge"://')
 gap=$(($height - $last_poc_challenge))
