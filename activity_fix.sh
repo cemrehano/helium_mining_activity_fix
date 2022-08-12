6691e6129ba1138b191a40706443a3215462e942
cemrehano <111056538+cemrehano@users.noreply.github.com>
2022-08-12 13:17:48 +0300

    Update activity_fix.sh

--- activity_fix.sh
+++ activity_fix.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 miner_name=$(docker exec miner miner print_keys|awk '/pubkey/ {split($1,a,"\""); print a[2]}')
+docker_name=$(docker ps -a|grep miner|awk -F" " '{print $NF}')
 height=$(curl -s https://api.helium.io/v1/blocks/height | grep -Po '"height":[^}]+' | sed -e 's/^"height"://')
 last_poc_challenge=$(curl -s https://api.helium.io/v1/hotspots/$miner_name |grep -Po '"last_poc_challenge":[^\,]+' | sed -e 's/^"last_poc_challenge"://')
 gap=$(($height - $last_poc_challenge))
@@ -8,7 +9,7 @@ echo
 if [ "$gap" -gt 200 ];
 then
     echo "No activity for at least 200 blocks\nRestarting miner service...";
-    docker restart miner
+    docker restart $docker_name
 else
     echo "Miner activity: Normal";
 fi;
